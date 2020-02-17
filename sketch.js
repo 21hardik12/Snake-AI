7b8aea2ced5025fe568776461f10a57dd53ec184
21hardik12 <54760905+21hardik12@users.noreply.github.com>
2020-02-17 20:47:12 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 let Boundries;
 let tileSize;
 let snake;
+let food;
 function setup() {
 	createCanvas(600, 600);
 	tileSize = 20;
@@ -10,15 +11,50 @@ function setup() {
 		[0, 0, width, tileSize],//upper
 		[0, height-tileSize, width, tileSize]//bottom
 	];
-	snake = new Snake(int(random(10, 20)), int(random(10, 20)), tileSize);
+	snake = new Snake(int(random(10, 20)) * tileSize, int(random(10, 20)) * tileSize, tileSize);
+	frameRate(15);
+	gen_food();
+}
+
+let left = [-1, 0];
+let right = [1, 0];
+let up = [0, -1];
+let down = [0, 1];
+let dir = right;
+function keyPressed() {
+	if (keyCode == UP_ARROW && dir != down) {
+		dir = up;
+	}
+	if (keyCode == DOWN_ARROW && dir != up) {
+		dir = down;
+	}
+	if (keyCode == RIGHT_ARROW && dir != left) {
+		dir = right;
+	}
+	if (keyCode == LEFT_ARROW && dir != right) {
+		dir = left;
+	}
+	snake.set_dir(dir);
+}
+
+function gen_food() {
+	food = createVector(int(random(2, 28)) * tileSize, int(random(2, 28)) * tileSize);
 }
 
 function draw() {
 	background(100);
 	// Drawing the Boundries;
 	for (let i = 0; i < Boundries.length; i++) {	
-		fill(250);
+		fill(0);
 		rect(Boundries[i][0], Boundries[i][1], Boundries[i][2], Boundries[i][3]);
 	}
+	// Drawing the food;
+	fill(255, 70, 10);
+	rect(food.x, food.y, tileSize, tileSize);
+	
+	snake.update();
 	snake.draw();
+	if (snake.eatFood(food)) {
+		gen_food();
+	}
 }
--- snake.js
+++ snake.js
@@ -2,20 +2,36 @@ class Snake {
 	constructor(x, y, tileSize) {
 		this.x = x;
 		this.y = y;
-		this.tail = [createVector(this.x, this.y), createVector(this.x+1, this.y), createVector(this.x+2, this.y)];
 		this.tileSize = tileSize;
-	}
+		this.dir = [1, 0];
+		this.xspeed = 1;
+		this.yspeed = 1;
+		this.total = 1;
+	}	
 	
-	draw() {
-		for (let i = 0; i < this.tail.length; i++) {
-			fill(230);
-			rect(this.tail[i].x * this.tileSize, this.tail[i].y * this.tileSize, this.tileSize, this.tileSize);
+	eatFood(food) {
+		let dis = dist(this.x, this.y, food.x, food.y);
+		if (dis < this.tileSize) {
+			this.total++;
+			return true;
+		} else {
+			return false;
 		}
 	}
 	
-	update() {
-		
-		
+	draw() {
+		fill(255);
+		rect(this.x, this.y, this.tileSize, this.tileSize);		
 	}
 	
+	set_dir(dir) {
+		this.dir = dir;
+	}
+	
+	update() {
+		this.x += this.xspeed * this.dir[0] * this.tileSize;
+		this.y += this.yspeed * this.dir[1] * this.tileSize;
+		this.x = constrain(this.x, this.tileSize, width-this.tileSize-this.tileSize);
+		this.y = constrain(this.y, this.tileSize, height-this.tileSize-this.tileSize);
+	}
 }
